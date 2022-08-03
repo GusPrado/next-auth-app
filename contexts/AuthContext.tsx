1ea69b82c9720f70dda6d60820e0ee86fe5be318
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-08-02 21:21:14 -0300

    feat(AuthContext): Add tokens to cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import Router from 'next/router';
 import { createContext, ReactNode, useState } from 'react';
+import { setCookie } from 'nookies';
 import { api } from '../services/api';
 
 type User = {
@@ -37,6 +38,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
       });
 
       const { permissions, roles, token, refreshToken } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
 
       setuser({
         email,
